20970b2abfd54de3fee12477ab2e0a5726694ea8
Simon Dobie <simondobie23@gmail.com>
2017-06-08 12:53:07 +1000

    Update travis.yml (#2)
    - updated travis
    - removed firefox from e2e

--- test/protractor/conf.js
+++ test/protractor/conf.js
@@ -2,12 +2,8 @@ let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec.js'],
   multiCapabilities: [
-    {
-      browserName: 'firefox'
-    },
     {
       browserName: 'chrome'
     }
